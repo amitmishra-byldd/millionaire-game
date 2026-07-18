b142b27596aa8ee705de2ed5f057c118effd038b
amitmishra-byldd <amit.mishra@byldd.com>
2026-07-19 00:19:43 +0530

    feat: implement wining amount

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,7 @@
 import Category from "./components/category-select/Category";
 import GamePlay from "./components/game-play/GamePlay";
+import Loss from "./components/game-play/Loss";
+import Win from "./components/game-play/Win";
 
 const routes = [
   {
@@ -9,6 +11,14 @@ const routes = [
   {
     path: "/game-play",
     element: <GamePlay />,
+  },
+  {
+    path: "/loss",
+    element: <Loss />,
+  },
+  {
+    path: "/win",
+    element: <Win />,
   },
 ];
 
